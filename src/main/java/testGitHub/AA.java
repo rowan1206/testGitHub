6c76ffdcf1a678cc92eb439283842e3bdf5d651f
rowan <rowan@yahoo.com>
2019-11-23 23:44:34 +0900

    aaeee

--- src/main/java/testGitHub/AA.java
+++ src/main/java/testGitHub/AA.java
@@ -3,7 +3,7 @@ package testGitHub;
 public class AA {
 	public static void main(String[] args) {
 		System.out.println("out");
-		System.out.println("out");
+		System.out.println("out push");
 		System.out.println("out");
 	}
 }
